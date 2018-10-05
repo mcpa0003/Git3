a2321c40d52934ae3907503805e99b11d1c8a570
Mike McPartlin <38846264+mcpa0003@users.noreply.github.com>
2018-10-05 01:33:18 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "9MbTR5l84Uzso0nBZBE2hZOas"
+consumer_secret = "nykKmN1tn1TsghtUcrvbp13NSBm5PMwIgazqldCgu1B61zNPAG"
+access_token = "1033956461137874944-kKmr6ncV9RBQkRPgF76iLYYvE162jN"
+access_token_secret = "endwUDx3ppANEFYh38WyXtDEILPYltBfhi5a59PVFSewv"
